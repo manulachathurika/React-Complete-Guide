a402b7e65e51a87a20994a9df5c64466574f8348
Manula Thantriwatte <manula.thantriwatte@moodys.com>
2019-03-28 12:19:11 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
       </div>
     );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this wrok now ?'));
+    
   }
 }
 
