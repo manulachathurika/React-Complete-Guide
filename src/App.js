644ff344aa7d300fe1144ab056cf0fb1e1560a6a
Manula Thantriwatte <manula.thantriwatte@moodys.com>
2019-04-02 12:24:16 +0530

    Passing method references

--- src/App.js
+++ src/App.js
@@ -16,12 +16,12 @@ const app = props => {
 
   console.log(personsState, otherState);
 
-  const switchNameHandler = () => {
+  const switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     // DON'T DO THIS: personsState.persons[0].name = 'Maximilian';
     setPersonsState({
       persons: [
-        { name: 'Maximilian', age: 28},
+        { name: newName, age: 28},
         { name: 'Manu', age: 29},
         { name: 'Stephanie', age: 27}
       ],
@@ -29,14 +29,33 @@ const app = props => {
     });
   };
 
+  const nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28},
+        { name: event.target.value, age: 29},
+        { name: 'Stephanie', age: 27}
+      ]
+    })
+  }
+
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is really working!</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+      <button onClick={() => switchNameHandler('Maximilian !!!')}>Switch Name</button>
+      <Person 
+        name={personsState.persons[0].name} 
+        age={personsState.persons[0].age}/>
+      <Person 
+        name={personsState.persons[1].name} 
+        age={personsState.persons[1].age}
+        click={switchNameHandler.bind(this, 'Max!')}
+        changed={nameChangeHandler}>My Hobbies: Racing
+      </Person>
+      <Person 
+        name={personsState.persons[2].name} 
+        age={personsState.persons[2].age}/>
     </div>
   );
   //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this wrok now ?'));
